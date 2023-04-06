21d55f05bd94b64147cf82ca5ff440b375fb48cb
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-06 17:43:59 +0300

    add/ variables and data types

--- index.php
+++ index.php
@@ -8,21 +8,37 @@
 </head>
 <body>
     <?php
-    echo '<b>hello</b> world!';
-    echo '<br>HELLO!';
+        echo "<h1>Vsriables</h1>";
 
-    echo '"';
+        $num = 5; //type: int
+        $num2 = -0.45; //type: float (floating point number)
+        $str = 'Variable: '; //type: string
+        $str2 = ' Variable 2: '; //type: string
+        $bool = true; //type: boolean
 
-// Comment 
+        echo $str .
+        $num .
+        '<br>' .
+        $str2 .
+        $num2 .
+        '<br>';
+;
 
-# Comment
+        $a = 0.5; // float
+        $b = '0.5'; // string
 
-/*
-Comment
-Comment
-Comment
-Comment
-*/
+        floatval($b); // now $b type is float 
+        echo $a + $b . '<br>'; // = 1 
+
+        define(MY_AGE, 21); //make constant
+        echo MY_AGE . '<br>'; //call const MY_AGE
+        echo "TheEnd";
+
+        /*
+        variables are not strongly typed,
+        which is considered minuses of php.
+        gotta keep that in mind
+        */
 
     ?>
 </body>
