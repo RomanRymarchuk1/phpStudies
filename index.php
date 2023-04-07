d215867a12047550d9b37bfea01084a38b818121
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-07 14:30:00 +0300

    add/ conditional operators

--- index.php
+++ index.php
@@ -8,25 +8,29 @@
 </head>
 <body>
     <?php
-        $str = 'Hello';
-        echo "Var: $str <br>"; //Variables in string
-        echo "<input type='text' value=\"aaaa\"><br>"; //2 ways to use quotes
+        $a = 2; 
+        $isBro = true;
 
-        $length = strlen($str); //func which return the string length
-        echo "$length <br>";
+        // if($a === 1) {
+        //     echo 'hi';
+        // } else {
+        //     echo 'bay';
+        // };
 
-        $trimmedText = trim('   some text      '); //func which return text whitout useless spaces 
-        echo  "$trimmedText <br>";
+/*
+ if there is only one line in the operator,
+ then you can not use {}
+*/
 
-        $lowerSrting = strtolower("$str");
-        echo  "$lowerSrting <br>";
-
-        $upperSrting = strtoupper($str);
-        echo  "$upperSrting <br>";
-
-        $hashSrting = md5($srt);
-        echo  "$hashSrting <br>";
-
+        if($a === 1)   
+            echo 'hi';
+        else if($a == 2 && $isBro) // and
+            echo 'brooooo';
+        else if($a == 3 || $isBro) // or
+            echo 'brooooo';
+        else 
+            echo 'bay';
+        
     ?>
 </body>
 </html>
