7c68e7b35da1ed973b8bd5bcc168a7ccc7241e28
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-07 13:57:33 +0300

    add/ string operations

--- index.php
+++ index.php
@@ -8,34 +8,24 @@
 </head>
 <body>
     <?php
-    $x = 10;
-    $y = 20;
+        $str = 'Hello';
+        echo "Var: $str <br>"; //Variables in string
+        echo "<input type='text' value=\"aaaa\"><br>"; //2 ways to use quotes
 
-    echo $x + $y . '<br>';
-    echo $x - $y . '<br>';
-    echo $x * $y . '<br>';
-    echo $x / $y . '<br>';
-    echo $x % $y . '<br>'; 
+        $length = strlen($str); //func which return the string length
+        echo "$length <br>";
 
-    $x += 10; // === $x = $x + 10;
-    $x -= 10; // === $x = $x - 10;
-    $x++; // === $x += 1;
-    $x--; // === $x -= 1;
+        $trimmedText = trim('   some text      '); //func which return text whitout useless spaces 
+        echo  "$trimmedText <br>";
 
-    // native php math constans
-    echo M_PI . '<br>'; // === 3.1415926535898
-    echo M_E . '<br>'; // === 2.718281828459
+        $lowerSrting = strtolower("$str");
+        echo  "$lowerSrting <br>";
 
-    // native php math functions
-    echo abs(22) . '<br>'; // |22| === 22
-    echo abs(-22) . '<br>'; // |-22| === 22
-    echo ceil(3.1) . '<br>'; // === 4 (round up)
-    echo floor(3.9) . '<br>'; // === 3 (round down)
-    echo round(3.9) . '<br>'; // === 4 
-    echo round(3.1) . '<br>'; // === 3 
-    echo mt_rand(0, 500) . '<br>'; // random num 0 - 500 
-    echo min(1, 2, 3, 4, 5, 6, 7, 8) . '<br>'; // === 1
-    echo max(1, 2, 3, 4, 5, 6, 7, 8) . '<br>'; // === 8
+        $upperSrting = strtoupper($str);
+        echo  "$upperSrting <br>";
+
+        $hashSrting = md5($srt);
+        echo  "$hashSrting <br>";
 
     ?>
 </body>
