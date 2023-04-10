9cffd1975d4c43577b39e3d3a53d6bfb2ef99505
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-10 17:13:46 +0300

    add/ work whith file system

--- index.php
+++ index.php
@@ -5,109 +5,63 @@
     <h1>Home</h1>
 <?php
 
-//______________________________________________________________________________
-//ARRAY METHODS
-//______________________________________________________________________________
+    $file = fopen("text.txt", "w"); //file open to rewrite or write somesing new
+
+    fwrite($file, "Exemple\nHello"); // write to file
+
+    fclose($file); //!!file MUST by closed!!
+
+    //OR
+
+    file_put_contents("text_2.txt", "Hello text_2\n"); // the same func in 1 str
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    $file_1 = fopen("text_1.txt", "a"); //file open to append
+
+    fwrite($file_1, "\nExemple\nHello"); // write to file 
+
+    fclose($file_1); //!!file MUST by closed!!
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    $fileName = 'text.txt';
+
+    $file_2 = fopen($fileName, 'r'); // file open for reading
+
+    $content = fread($file_2, filesize($fileName));
+
+    fclose($file_2); //!!file MUST by closed!!
+
+    echo $content . '<br>';
+
+    //OR
+
+    echo file_get_contents($fileName) . '<br>';// the same func in 1 str
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    //checks for the existence of a file by its name
+    echo file_exists('text.txt') . '<br>'; //1
+    echo file_exists('t.txt') . '<br>'; //''
+
+    //use to rnemae file
+    //rename('text_1.txt', 'text___1.txt');
+
+    //use to delete file
+    // unlink('text.txt');
+
+    //show the path to current file
+    echo __FILE__ . "<br>";
+
+    //show the access rights 
+    echo fileperms(__FILE__) . "<br>";
+
+    //change the access rights 
+    chmod(__FILE__, 0777);
 
 
-    $list = [3, 5, 7, 90, 23, 75,];
-
-    // delete element by index, but else elements saves there index
-    unset($list[0]); 
-
-    //reset index for elements, and return new array
-    $list = array_values($list); 
-
-    //sorts the array in ascending order
-    sort($list); 
-
-    //sorts the array in descending order
-    rsort($list); 
-
-    //sorts the array in random order
-    shuffle($list); 
-
-    //serchin element in array, return 1 == true; '' == false
-    in_array(3, $list); 
-
-     //slice the array from index to index and return in new array
-    $arr = array_slice($list, 0, 3);
-
-    //show array langth
-    echo count($arr) . '<br>'; 
-
-    //merge 2 arrays
-    $arr_1 = [1, 3, 4, 5];
-    $arr_2 = [12, 33, 44, 45];
-    $arr_3 = array_merge($arr_1, $arr_2);
-    print_r($arr_3);
-    echo '<br>';
-
-    //show array on the host
-    print_r($list); 
-    echo '<br>';
-
-
-// ______________________________________________________________________________
-// TYPES
-// ______________________________________________________________________________
-
-    $x = 13;
-
-    //return data type
-    echo gettype($x); // type int
-    echo '<br>';
-
-    $y = floatval('15');
-
-    echo gettype($x); // type Duble
-    echo '<br>';
-
-    //return 1 when arg is num and '' in other case
-    echo is_numeric(2); //1
-    echo is_numeric(2.2); //1
-    echo is_numeric('2.2'); //1
-    echo is_numeric('str'); //''
-    echo '<br>';
-
-    //return 1 when arg is int and '' in other case
-    echo is_integer(100); // 1
-    echo is_integer(100.3); // ''
-    echo is_integer('str'); //''
-    echo is_integer('2.2'); //''
-    echo '<br>';
-
-    //return 1 when arg is string and '' in other case
-    is_string('qwerty'); //1
-
-    //return 1 when arg is float and '' in other case
-    is_double(2.2); //1
-
-    //return 1 when arg is boolean and '' in other case
-    is_bool(false); //1
-
-    //return 1 when arg is array and '' in other case
-    is_array([false, 2, 'tyu']); //1
-
-//______________________________________________________________________________
-// STRING
-// ______________________________________________________________________________
-
-    $str = 'Some STR';
-
-    //looking for match in the string
-    echo strpos($str, 'ST');
-    echo '<br>';
-
-    //split string by given parameter and return new array
-    $str_1 = 'Sam, Roman, Bob, Mark';
-    $names_array = explode(",", $str_1);
-    print_r($names_array);
-    echo '<br>';
-
-    //concatenates an array into a string with the specified delimiter
-    echo implode(' | ', $names_array);
-    echo '<br>';
+    
 
     require_once("./blocks/footer.php");
 ?>
