13e19485fbf509a475ad5c1f17e7baf7c57f8ba9
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-11 16:14:19 +0300

    add/ send mail

--- index.php
+++ index.php
@@ -4,25 +4,14 @@
 ?>
     <h1>Home</h1>
 <?php
-    // phpinfo(); //ALL info about php, plugins, extentions and other...
+    $message = "Message";
+    $to = "roman.rymarchuk01@gmail.com";
+    $from = "eeeeeee.nnnnnnn2014@yandex.ru";
 
-    // echo '<pre>'; // All SERVER information
-    // print_r($_SERVER);
-    // echo '</pre>';
+    $subject = "=?utf-8?B?".base64_encode("Title")."?="; // encode the subject of the letter MANDATORY
+    $headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";//add headers to mail
 
-    echo $_SERVER['HTTPS'] . "<br>"; // secure connection check
-    echo $_SERVER['HTTP_HOST'] . "<br>"; //return domain name
-    echo $_SERVER['REQUEST_URI'] . "<br>"; // return all info after domain
-    echo $_SERVER['HTTP_USER_AGENT'] . "<br>"; //return users OS name
-
-    if($_GET['source'] != '' ){
-        $link = explode('?source=', $_SERVER['REQUEST_URI']);
-        $redirect = "http://$_SERVER[HTTP_HOST]$link[0]";
-
-        header("HTTP/1.1 301: Moved Permanently");
-        header("Location: $redirect");
-        exit();
-    }
+    mail($to, $subject, $message, $headers); //dont work on localhost
 
     require_once("./blocks/footer.php");
 ?>
