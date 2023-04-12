bdf523f572286ccd8ae5af42bf23dfe3ad3b7cca
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-12 14:37:32 +0300

    add/ cookie and session

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
+    session_start(); // session MUST be startetd ahead of rendering anything on the page
+
     $title = 'Home';
     require_once("./blocks/header.php");//connect once
 ?>
     <h1>Home</h1>
-<?php
-    $message = "Message";
-    $to = "roman.rymarchuk01@gmail.com";
-    $from = "eeeeeee.nnnnnnn2014@yandex.ru";
+<?php  
 
-    $subject = "=?utf-8?B?".base64_encode("Title")."?="; // encode the subject of the letter MANDATORY
-    $headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";//add headers to mail
+//_____________________________________________________________________________________________________________
+//COOKIE
+//_____________________________________________________________________________________________________________
 
-    mail($to, $subject, $message, $headers); //dont work on localhost
+    $user_name = "Alex";
+    setcookie('user_name', $user_name, time() + 180); //setcookie(key, value, liveTime)
+    // setcookie('user_name', $user_name, time() - 180); //way to delete cookie
+
+    // print_r($_COOKIE);
+    echo $_COOKIE['user_name'] . "<br>";
+
+//_____________________________________________________________________________________________________________
+//SESSION
+//_____________________________________________________________________________________________________________
+
+    $_SESSION['user_name'] = $user_name;
+
+    if($_SESSION["user_name"] == "Alex"){
+        echo "ohohoh $_SESSION[user_name]";
+    }
+
+    unset($_SESSION["user_name"]); // delete one element of session
+    session_destroy(); // delete ALL session
 
     require_once("./blocks/footer.php");
 ?>
